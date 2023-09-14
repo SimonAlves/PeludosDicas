51d6375355de1ffe1df03bc64ae3513d6d0bad38
SimonAlves <123409031+SimonAlves@users.noreply.github.com>
2023-09-13 23:45:47 -0300

    atualização botão de sair e usuario e senha.

--- app/src/main/java/com/example/peludosdicas/MainActivity.kt
+++ app/src/main/java/com/example/peludosdicas/MainActivity.kt
@@ -1,22 +1,32 @@
 package com.example.peludosdicas
 
+import android.app.Instrumentation.ActivityResult
 import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.JsonToken
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.peludosdicas.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlin.math.sign
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var googleSinInCliente: GoogleSignInClient
+    private lateinit var googleSinInClient: GoogleSignInClient
     private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +37,13 @@ class MainActivity : AppCompatActivity() {
 
         auth = Firebase.auth
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("179574771008-7nn0e9k6siovgfi5ij3snpee8iqdvaf4.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        googleSinInClient = GoogleSignIn.getClient(this,gso)
+
+
         binding.botaoEntrar.setOnClickListener {
 
             if (TextUtils.isEmpty(binding.editTextTextUsuario.text)) {
@@ -34,8 +51,8 @@ class MainActivity : AppCompatActivity() {
                     "Campo usuário não pode estar em branco"
 
             } else if (TextUtils.isEmpty(binding.editTextSenha.text)) {
-                    binding.editTextSenha.error =
-                        "Campo senha não pode estar em branco"
+                binding.editTextSenha.error =
+                    "Campo senha não pode estar em branco"
             } else {
 
                 loginEusuarioEsenha(
@@ -46,7 +63,58 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+        binding.botaoGoogle.setOnClickListener {
+
+            signIn()
+        }
+
     }
+
+    private fun signIn() {
+        val intent = googleSinInClient.signInIntent
+        abreActivity.launch(intent)
+    }
+   var abreActivity = registerForActivityResult(
+       ActivityResultContracts.StartActivityForResult()
+   ) {
+    result : androidx.activity.result.ActivityResult ->
+       if(result.resultCode == RESULT_OK){
+           val intent = result.data
+            val task = GoogleSignIn.getSignedInAccountFromIntent(intent)
+           try {
+               val conta = task.getResult(ApiException::class.java)
+               loginComGoogle(conta.idToken!!)
+
+           } catch (exception: ApiException){
+
+           }
+       }
+
+   }
+
+    private fun loginComGoogle(token: String){
+        val credencial = GoogleAuthProvider.getCredential(token, null)
+        auth.signInWithCredential(credencial).addOnCompleteListener(this)
+        {
+          task : Task<AuthResult> ->
+            if(task.isSuccessful){
+                Toast.makeText(
+                    baseContext, "autenticação efetuada com o Google.",
+                    Toast.LENGTH_SHORT).show()
+                abrePrincipal()
+
+            }else{
+                Toast.makeText(
+                    baseContext, "erro de autenticação com o Google.",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+        }
+    }
+
+
+
 
     private fun loginEusuarioEsenha(usuario:String,senha:String) {
         auth.signInWithEmailAndPassword(
@@ -56,7 +124,6 @@ class MainActivity : AppCompatActivity() {
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
-                    val user = auth.currentUser
                     Toast.makeText(
                         baseContext, "autenticação efetuada.",
                         Toast.LENGTH_SHORT).show()
